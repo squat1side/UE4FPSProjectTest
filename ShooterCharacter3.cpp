068afb62c25e39dda3997a5203ec4e505fd26e66
squat1side <squat1side@163.com>
2022-10-28 22:50:03 +0800

    ShooterCharacter_fireparticle

---
+++ ShooterCharacter3.cpp
@@ -0,0 +1,207 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ShooterCharacter.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Sound/SoundCue.h"
+#include "Engine/SkeletalMeshSocket.h"
+#include "DrawDebugHelpers.h"
+#include "Particles/ParticleSystemComponent.h"
+
+// Sets default values
+AShooterCharacter::AShooterCharacter():
+
+	//初始化基础速率
+	BaseTurnRate(45.f),
+	BaseLookUpRate(45.f)
+
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	//实现将摄像机杆设置为默认子类组件，并附加在根组件,并设置臂长，设置摄像机跟随视角
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->TargetArmLength = 300.f;
+	CameraBoom->bUsePawnControlRotation = true;
+	//设置插槽偏移 - 使角色不在屏幕中间  -使摄像机组件设置一些偏移量
+	CameraBoom->SocketOffset = FVector(0.f, 50.f, 50.f);
+
+
+
+	//创建CameraFollow
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	//摄像机附加在弹簧臂的末端插槽
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	//摄像机跟随吊杆旋转
+	FollowCamera->bUsePawnControlRotation = false;
+
+	//控制器只影响摄像机，不影响角色的转动
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = true;
+	bUseControllerRotationRoll = false;
+
+	//设置角色的移动
+	GetCharacterMovement()->bOrientRotationToMovement = false;//角色在输入方向上移动
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 540.f, 0.f);//旋转速度
+	//设置角色跳跃
+	GetCharacterMovement()->JumpZVelocity = 600.f;
+	GetCharacterMovement()->AirControl = 0.2f;
+}
+
+// Called when the game starts or when spawned
+void AShooterCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+//实现MoveForward
+void AShooterCharacter::MoveForward(float Value)
+{
+	//检查继承的控制器的变量是否有效
+	if ((Controller != nullptr) && (Value != 0.0f))
+	{
+		//找出那个方向是前进{初始化}
+		const FRotator Rotation{ Controller->GetControlRotation() };
+		const FRotator YawRotation{ 0, Rotation.Yaw, 0 };
+
+		const FVector Direction{ FRotationMatrix{YawRotation}.GetUnitAxis(EAxis::X) };
+		//添加移动输入 -移动角色和移动组件中的其他信息确定
+		AddMovementInput(Direction, Value);
+	}
+}
+
+//实现MoveRight
+void AShooterCharacter::MoveRight(float Value)
+{
+	//检查继承的控制器的变量是否有效
+	if ((Controller != nullptr) && (Value != 0.0f))
+	{
+		//找出那个方向是左右转向{初始化}
+		const FRotator Rotation{ Controller->GetControlRotation() };
+		const FRotator YawRotation{ 0, Rotation.Yaw, 0 };
+
+		const FVector Direction{ FRotationMatrix{YawRotation}.GetUnitAxis(EAxis::Y) };
+		//添加移动输入 -移动角色和移动组件中的其他信息确定
+		AddMovementInput(Direction, Value);
+	}
+}
+
+//实现视角转动来影响控制器
+void AShooterCharacter::TurnAtRate(float Rate)
+{
+	//根据速率信息计算每一帧的Delta
+	AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds());
+}
+
+void AShooterCharacter::LookUpAtRate(float Rate)
+{
+	AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
+}
+
+//创建武器开火功能：
+void AShooterCharacter::FireWeapon()
+{
+	//判断开火音效是否有效
+	if (FireSound)
+	{
+		//调用play sound 2D -静态函数 - 游戏的play是静态的
+		UGameplayStatics::PlaySound2D(this, FireSound);
+	}
+	//获取插槽 - 创建一个局部变量
+	const USkeletalMeshSocket* BarrelSocket = GetMesh()->GetSocketByName("BarrelSocket");
+	if (BarrelSocket)
+	{
+		const FTransform SocketTransform = BarrelSocket->GetSocketTransform(GetMesh());
+		//生成粒子特效
+		if (MuzzleFlash)
+		{
+			UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), MuzzleFlash, SocketTransform);
+		}
+		//Performing the Line Trace -执行跟踪轨迹
+		FHitResult FireHit;
+		const FVector Start{ SocketTransform.GetLocation() };
+		const FQuat Rotation{ SocketTransform.GetRotation() };
+		const FVector RotationAxis{ Rotation.GetAxisX() };
+		const FVector End{ Start + RotationAxis * 50000.f };
+
+		FVector BeamEndPoint{ End };
+
+		GetWorld()->LineTraceSingleByChannel(FireHit, Start, End, ECollisionChannel::ECC_Visibility);
+		if (FireHit.bBlockingHit)
+		{
+			//Debug调试线
+			//DrawDebugLine(GetWorld(), Start, End, FColor::Red, false, 2.f);
+			//DrawDebugPoint(GetWorld(), FireHit.Location, 5.f, FColor::Red, false, 2.f);
+
+			//光束粒子的粒子系统
+			BeamEndPoint = FireHit.Location;
+
+			//检查子弹撞击粒子是否有效
+			if (ImpactParticles)
+			{
+				UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), ImpactParticles, FireHit.Location);
+			}
+		}
+		//烟雾轨迹
+		if (BeamParticles)
+		{
+			UParticleSystemComponent* Beam = UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), BeamParticles, SocketTransform);
+			if (Beam)
+			{
+				Beam->SetVectorParameter(FName("Target"), BeamEndPoint);
+			}
+		}
+	}
+	//创建一个局部变量动画实例 - 用于控制蒙太奇中开火动画
+	UAnimInstance* AnimInstance = GetMesh()->GetAnimInstance();
+	if (AnimInstance && HipFireMontage)
+	{
+		AnimInstance->Montage_Play(HipFireMontage);
+		AnimInstance->Montage_JumpToSection(FName("StartFire"));
+	}
+}
+
+
+
+// Called every frame
+void AShooterCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AShooterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	//使用check宏检查输入组件是否有效
+	check(PlayerInputComponent);
+
+	//将移动函数绑定到轴映射
+	PlayerInputComponent->BindAxis("MoveForward", this, &AShooterCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AShooterCharacter::MoveRight);
+
+	//将转向速率函数绑定到轴映射
+	PlayerInputComponent->BindAxis("TurnRate", this, &AShooterCharacter::TurnAtRate);
+	PlayerInputComponent->BindAxis("LookUpRate", this, &AShooterCharacter::LookUpAtRate);
+
+	//将鼠标转向速率函数绑定到轴映射
+	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
+
+	//将jump函数绑定在动作映射
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this,  &ACharacter::Jump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
+
+	//将武器开火绑定在动作映射
+	PlayerInputComponent->BindAction("FireButton", IE_Pressed, this, &AShooterCharacter::FireWeapon);
+
+}
+
+
---
+++ ShooterCharacter3.h
@@ -0,0 +1,91 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "ShooterCharacter.generated.h"
+
+UCLASS()
+class SHOOTER_API AShooterCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AShooterCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	//向前和向后轴映射输入
+	void MoveForward(float Value);
+
+	//向左向右的轴映射输入
+	void MoveRight(float Value);
+
+	//给定的速率来转动
+	void TurnAtRate(float Rate);
+
+	void LookUpAtRate(float Rate);
+
+	//创建武器开火功能：
+	void FireWeapon();
+
+
+
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	//将摄像机吊杆设置在角色与摄像机中间
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* CameraBoom;
+
+	//设置摄像机跟随角色
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* FollowCamera;
+
+	//基础转向速率 - 度/秒，其他缩放会影响最终旋转速率
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	float BaseTurnRate;
+
+	//基础上下速率 - 度/秒，其他缩放会影响最终旋转速率
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	float BaseLookUpRate;
+
+	//添加fire weapon的声效变量
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Combat, meta = (AllowPrivateAccess = "true"))
+	class USoundCue* FireSound;
+
+	//添加开火枪口粒子系统变量
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Combat, meta = (AllowPrivateAccess = "true"))
+	class UParticleSystem* MuzzleFlash;
+
+	//控制蒙太奇中开火动画
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Combat, meta = (AllowPrivateAccess = "true"))
+	class UAnimMontage* HipFireMontage;
+
+	//冲击粒子在子弹射击过程的撞击点
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Combat, meta = (AllowPrivateAccess = "true"))
+	UParticleSystem* ImpactParticles;
+
+	//添加一个光束粒子的烟雾轨迹粒子系统
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Combat, meta = (AllowPrivateAccess = "true"))
+	UParticleSystem* BeamParticles;
+
+
+public:
+	//返回一个摄像机吊杆子对象 -设置CameraBoom的Getter
+	FORCEINLINE USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+
+	//返回一个新的摄像机组件指针，将其称为获取跟随相机 -设置Camera的getter
+	FORCEINLINE UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+
+};
